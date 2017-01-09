49cca4220a6dcac7c6e85909376089b4753b221c
Tyler Torola <tyler@tjt.codes>
2017-01-09 10:24:07 -0800

    starts implamenting store in main func

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+const configureStore    = require('store/configure.js'),
+      { applySettings } = require('store/actions.js');
+
 module.exports = settings => horizon => {
-	// init store
+	const store = configureStore();
+	store.dispatch(applySettings(settings));
 	// configure store with settings
 	// listen with renderer
 	// listen with stack watcher
