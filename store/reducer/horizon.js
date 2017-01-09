3f4054d5ebb012cfd1ef9b150582b43935b1a1a1
Tyler Torola <tyler@tjt.codes>
2017-01-09 10:43:56 -0800

    adds horizon control actions

--- store/reducer/horizon.js
+++ store/reducer/horizon.js
@@ -2,6 +2,11 @@ module.exports = (horizon = [], action) => {
 	switch (action.type) {
 		case "SET_HORIZON":
 			return action.horizon;
+		case "PUSH_HORIZON":
+			return [action.url, ...horizon];
+		case "POP_HORIZON":
+			const [first, ...rest] = horizon;
+			return rest;
 	}
 	return state;
 };
