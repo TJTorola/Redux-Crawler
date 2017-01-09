81e18c17a0d2f0d71abcd5188f0830fb85fb4081
Tyler Torola <tyler@tjt.codes>
2017-01-09 10:00:58 -0800

    psuedo-coded main function

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 module.exports = settings => horizon => {
-	console.log(horizon);
+	// init store
+	// configure store with settings
+	// listen with renderer
+	// listen with stack watcher
+	// send horizon to start process
 }
