08dba897bcf4bff3facfa0a00493b1b66714454e
Tyler Torola <tyler@tjt.codes>
2017-01-09 10:39:32 -0800

    adds render stub

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const configureStore    = require('./store/configure.js'),
-      { applySettings } = require('./store/actions.js');
+      { applySettings } = require('./store/actions.js'),
+      render            = require('./lib/render.js');
 
 module.exports = settings => horizon => {
 	const store = configureStore();
+
 	store.dispatch(applySettings(settings));
-	// configure store with settings
-	// listen with renderer
+
+	store.subscribe(render(store));
+
 	// listen with stack watcher
 	// send horizon to start process
 }
