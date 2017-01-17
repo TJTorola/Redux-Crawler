e77f3bbdd62a90421c3eff6e93018dae2b15e4ff
Tyler Torola <tyler@tjt.codes>
2017-01-17 15:44:35 -0800

    modifying basic config options

--- config.js
+++ config.js
@@ -1,19 +1,29 @@
-const mapper = result => {
-	const stripped = result.replace(/[-.\(\)\\ ]/g, ''),
-	      areacode = stripped.slice(0, 3),
-	      prefix   = stripped.slice(3, 6),
-	      postfix  = stripped.slice(6);
+const mapper = {
+	phone(results) {
+		const phoneNumber = number => {
+			const stripped = result.replace(/[-.\(\)\\ ]/g, ''),
+			      areacode = stripped.slice(0, 3),
+			      prefix   = stripped.slice(3, 6),
+			      postfix  = stripped.slice(6);
 
-	return `(${ areacode }) ${ prefix }-${ postfix }`;
-}
+			return `(${ areacode }) ${ prefix }-${ postfix }`;
+		}
 
-// email        : \b\w+@\w+\.\w{2,6}\b
-// phone-strict : \(?\b[0-9]{3}\)?[-. \\][0-9]{3}[-. \\][0-9]{4}\b
-// phone        : \(?\b[0-9]{3}\)?[-. \\]?[0-9]{3}[-. \\]?[0-9]{4}\b
+		return results.map(phoneNumber);
+	},
+
+	basic(results) { return results; }
+};
+
+const regex = {
+	email: /\b\w+@\w+\.\w{2,6}\b/g,
+	phone: /\(?\b[0-9]{3}\)?[-. \\]?[0-9]{3}[-. \\]?[0-9]{4}\b/g,
+	phoneStrict: /\(?\b[0-9]{3}\)?[-. \\][0-9]{3}[-. \\][0-9]{4}\b/g
+};
 
 module.exports = {
 	swarmSize    : 16,
 	horizonLimit : 100,
-	parseRegex   : /\(?\b[0-9]{3}\)?[-. \\]?[0-9]{3}[-. \\]?[0-9]{4}\b/g,
-	parseMapper  : results => results.map(mapper)
+	parseRegex   : regex['email'],
+	parseMapper  : mapper['basic']
 };
